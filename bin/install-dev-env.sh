a515feb09daeae8cdfb4d7a4d0d92e69b47d628b
Ranfis Sanchez <ranfis.sh@gmail.com>
2013-12-14 00:01:14 -0400

    Add dev instalation script

---
+++ bin/install-dev-env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get -y install git npm tree
+
+# the above will install nodejs too
+
+# Install N (node version manager)
+#https://github.com/visionmedia/n
+
+sudo npm install -g n
+# To install specific node version do the following
+#sudo n 0.10.6
+sudo n stable
+
+sudo npm install -g mocha
+sudo npm install -g coffee-script
+sudo npm install -g grunt
+sudo npm install -g browserify
